3630c6d7c1a5435b24a4a0cd6c75bf5a09545124
yuntelee <jyl2199@columbia.edu>
2026-03-06 15:15:08 -0500

    Remove unused Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
-  },
 };
 
 export default nextConfig;
